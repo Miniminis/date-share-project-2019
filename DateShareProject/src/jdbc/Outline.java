f3a87d54a1cb2219e06d41dc48ccd1d2dcc3e7e4
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-07-16 17:05:43 +0900

    [DateShareProject] ver 1.4 프로젝트 개요도 작성

---
+++ DateShareProject/src/jdbc/Outline.java
@@ -0,0 +1,52 @@
+package jdbc;
+
+public class Outline {}
+
+/* Project Outline 
+ * /JavaResources/src 
+ * 1. dateShare.Dao
+ * - dateUserDao.java 
+ * - activityDao.java 
+ * - foodDao.java 
+ * - movieDao.java
+ * - messageDao.java
+
+ * 2. dateShare.Exception
+ * - InvalidMessagePasswordException
+ * - MessageNotFoundException 
+ * - 향후 추가 
+ * 
+ * 
+ * 3. dateShare.Model (각 DB 테이블과 매핑되는 클래스)
+ * - DateUser.java : 회원 테이블
+ * - Food.java : 맛집 테이블
+ * - Movie.java : 영화추천 테이블 
+ * - Activity.java : 엑티비티 테이블 
+ * - Message.java : 쪽지 테이블 
+ * 
+ * 4. dateShare.Service
+ * - 각 부분별 필요한 서비스 자유롭게 추가 
+ * 
+ * 
+ * 5. jdbc
+ * - ConnectionProvider
+ * - DBCPInit
+ * - jdbcUtil
+ * - Outline: 프로젝트 전체 구조 설명 참고파일
+ * 
+ * /WebContent 
+ * 메인 페이지: main.jsp
+ * 
+ * 1. css
+ * - index.css
+ * 
+ * 2. frame
+ * - header.jsp 
+ * - nav.jsp
+ * - my.jsp
+ * - footer.jsp 
+ * 
+ * 3. member 
+ * - 각자 필요한 페이지에 따라 자유롭게 추가 
+ * 
+ * */
